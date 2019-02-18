f4a424f47f67d57338c1acd15556579b6eb5372a
m0112365 <m01123652c@edu.teu.ac.jp>
2019-02-18 16:33:52 +0900

    テスト

---
+++ esp/kaito/ice edit/testg.py
@@ -0,0 +1,17 @@
+file = open("processed_code.txt", "w")
+
+
+with open("to_process_code.txt") as gcode_file_original:
+    contents = gcode_file_original.readlines()
+    for line in contents:
+        line = str(line)
+        get = line.find("Z")
+
+        if get != -1:
+            line = "\n" + "G94" + line + "\n" + "G93" + "\n"
+            print(line)
+            file.write(line)
+        else:
+            line = line
+            print(line)
+            file.write(line)
